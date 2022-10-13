2ca6fb05a1b7e0c4d4406669816c7c36b6c58b26
Pentapatch <newside-sys@hotmail.com>
2022-10-13 12:41:20 +0200

    Grundstate

--- Program.cs
+++ Program.cs
@@ -1,3 +1,32 @@
+// 21:an (Blackjack)
+
+// -> Att generera slumptal
+// -> Planera projektet
+// -> Skriva pseudo kod för projektet
+// -> Göra spelet
+// -> Förslag på att förbättra koden
+
+// ###############
+// ## PSEUDOKOD ##
+// ###############
+
+// Skriv välkomstmeddelande
+
+// Sätt menyVal till "0"
+
+// while (menyval != ”4”)
+
+// Skriv ut meny
+
+// Läs in menyVal
+
+// switch menyVal
+// case 1: Spela en omgång av 21:an
+// case 2: Visa senaste vinnaren
+// case 3: Visa spelets regler
+// case 4: Gör ingenting (programmet avslutas)
+// default: Skriv att alternativet var ogiltigt
+
 ﻿namespace BlackJack
 {
     internal class Program
@@ -102,4 +131,3 @@
         }
     }
 }
-
