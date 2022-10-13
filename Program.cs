8e05cf88330e08e7b4e7fa7cfbcdf6408f95c0e7
Pentapatch <newside-sys@hotmail.com>
2022-10-13 12:15:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// 21:an (Blackjack)
+
+// -> Att generera slumptal
+// -> Planera projektet
+// -> Skriva pseudo kod för projektet
+// -> Göra spelet
+// -> Förslag på att förbättra koden
+
+// * Spela mot datorn
+
