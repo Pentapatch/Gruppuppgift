900bd3862e5ea1aa7882c9e661d54fc50317388e
Pentapatch <newside-sys@hotmail.com>
2022-10-13 12:36:02 +0200

    init

--- Program.cs
+++ Program.cs
@@ -6,5 +6,26 @@
 // -> Göra spelet
 // -> Förslag på att förbättra koden
 
-// * Spela mot datorn
+// ###############
+// ## PSEUDOKOD ##
+// ###############
+
+// Skriv välkomstmeddelande
+
+// Sätt menyVal till "0"
+
+// while (menyval != ”4”)
+
+// Skriv ut meny
+
+// Läs in menyVal
+
+// switch menyVal
+// case 1: Spela en omgång av 21:an
+// case 2: Visa senaste vinnaren
+// case 3: Visa spelets regler
+// case 4: Gör ingenting (programmet avslutas)
+// default: Skriv att alternativet var ogiltigt
+
+
 
